8564fa51587960e3794810f60283158799a663cf
Als1510 <lekhrajsharmaas@gmail.com>
2021-10-09 00:20:26 +0530

    Modified Iterative DFS Traversal in matrix

--- C++/Algorithm/Searching/dfsIterative.cpp
+++ C++/Algorithm/Searching/dfsIterative.cpp
@@ -4,36 +4,37 @@
   Description : DFS Traversal in 2D matrix
 */
 
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
-int n;
-int m;
-int vis[1001][1001];
-int grid[1001][1001];
-int dx[4] = {-1, 0, 1, 0};
-int dy[4] = {0, 1, 0, -1};
-
-void dfs(int x, int y) {
-  cout << grid[x][y] << " ";
-  vis[x][y] = 1;
-  for (int i = 0; i < 4; i++) {
-    int u = x + dx[i];
-    int v = y + dy[i];
-    if (u >= 0 && u < n && v >= 0 && v < m && vis[u][v] == 0) {
-      dfs(u, v);
+int main(){
+    int n;
+    cin>>n;
+    int c[n][n];
+    int i,j;
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            cin>>c[i][j];
+        }
     }
-  }
-}
-
-int main() {
-  cin >> n >> m;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      cin >> grid[i][j];
-      vis[i][j] = 0;
+    int dfs[n];
+    for(i=0;i<n;i++){
+        dfs[i]=-1;
     }
-  }
-  dfs(0, 0);
-  return 0;
+    dfs[0]=1;
+    j=1;
+    for(i=0;i<n;i++){
+        if(dfs[i]==1&&i>j)
+        cout<<i;
+        dfs[i]=1;
+        while(c[i][j]!=0&&j<n&&i<n){
+            if(dfs[j]==-1)
+            {
+                dfs[j]=1;
+            }
+            if(dfs[j]==1){
+                cout<<j;
+                j++;
+            }
+        }
+    }
 }
